54caf6c463f5c0a31289c6ce7c0dc4f20ad8ff8c
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-08-24 01:12:10 -0700

    Revamped to use modules instead of building manually

--- ohpc-recipe.py
+++ ohpc-recipe.py
@@ -18,15 +18,10 @@ Stage0 += shell(commands=["sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/
 
 Stage0 += shell(commands=['yum update -y',
                           'rm -rf /var/cache/yum/*'])
-
-Stage0 += packages(epel=True, powertools=True,
-                   yum=['make gcc wget openssl-devel bzip2-devel libffi-devel'])
-
 # Install Python 3.7
-Stage0 += shell(commands=['cd /tmp/', 'wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz', 
-    'tar xzf Python-3.7.9.tgz', 'cd Python-3.7.9', './configure --enable-optimizations', 
-    'make altinstall', 'ln -sfn /usr/local/bin/python3.7 /usr/bin/python3.7', 'ln -sfn /usr/local/bin/pip3.7 /usr/bin/pip3.7'])
+Stage0 += packages(epel=True,
+                   yum=['python38'])
 
-Stage0 += pip(packages=['keras==2.6.0', 'tensorflow==2.6.0', 'protobuf==3.20.*'], pip='pip3.7')
+Stage0 += pip(packages=['keras==2.6.0', 'tensorflow==2.6.0', 'protobuf==3.20.*'], pip='pip3.8')
 
 
