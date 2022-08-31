f060beb1ff9592bd5f85909006c76e6f033f3565
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-08-31 00:57:56 -0700

    Use anaconda to install python and nvidia drivers

--- README.md
+++ README.md
@@ -4,22 +4,50 @@
 This is a container recipe for [NVIDIA's HPC container maker](https://github.com/NVIDIA/hpc-container-maker). The base image is [OpenHPC's development environment](https://quay.io/repository/ohpc/ohpc-gnu9), with added Python, TensorFlow, and Keras support
 
 
+
+---
+[share]$ nvcc --version
+nvcc: NVIDIA (R) Cuda compiler driver
+Copyright (c) 2005-2015 NVIDIA Corporation
+Built on Tue_Aug_11_14:27:32_CDT_2015
+Cuda compilation tools, release 7.5, V7.5.17
+
+module: loading 'cuda/7.5.18'
+
+---
+
+
+tensorflow-2.6.0
+cuDNN 8.1	
+cuda 11.2
+
+
+in sbatch script, 
+module load cuda/11.3.1
+module load cudnn/8.1.0
+
+
+https://www.tensorflow.org/install/source
+
+
 ## Usage examples
 
 
 #### Docker
 
 ```bash
-hpccm --recipe recipes/ohpc-recipe.py --format docker > Dockerfile
+hpccm --recipe ohpc-recipe.py --format docker > Dockerfile
 docker build -t ohpc-recipe -f Dockerfile .
 docker run -v $PWD/python_scripts/:/mnt/python_scripts/ -it --rm ohpc-recipe python3.7 /mnt/python_scripts/test.py
 ```
 
 #### Singularity
 
+Note: For singularity builds, root access is required. If you are on MacOS or Windows, please check out the instructions [here](https://docs.sylabs.io/guides/3.0/user-guide/installation.html#mac) on how to use Vagrant to build a Singularity virtual machine
+
 ```bash
-hpccm --recipe recipes/ohpc-recipe.py --format singularity > Singularity.def
-singularity build ohpc-recipe.simg Singularity.def
+hpccm --recipe ohpc-recipe.py --format singularity > Singularity.def
+sudo singularity build ohpc-recipe.simg Singularity.def
 singularity exec --nv ohpc-recipe.simg python3.7 $PWD/python_scripts/benchmark.py
 ```
 
--- ohpc-recipe.py
+++ ohpc-recipe.py
@@ -3,7 +3,7 @@
 Sample workflow:
 $ hpccm.py --recipe recipes/ohpc.py --format docker > Dockerfile
 $ docker build -t ohpc-recipe -f Dockerfile .
-$ docker run -v $PWD/python_scripts/:/mnt/python_scripts/ -it --rm ohpc-recipe python3.7 /mnt/python_scripts/benchmark.py
+$ docker run -v $PWD/python_scripts/:/mnt/python_scripts/ -it --rm ohpc-recipe python3 /mnt/python_scripts/benchmark.py
 """
 # pylint: disable=invalid-name, undefined-variable, used-before-assignment
 import os
@@ -19,19 +19,22 @@ Stage0 += shell(commands=["sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/
 Stage0 += shell(commands=['yum update -y',
                           'rm -rf /var/cache/yum/*'])
 
-Stage0 += packages(epel=True,
-                   yum=['wget', 'python38'])
+#  Stage0 += packages(epel=True,
+#                     yum=['wget', 'python38'])
+
+Stage0 += conda(eula=True, 
+        packages=['keras==2.6.0', 'tensorflow==2.6.0'])
 
 # Install Python 3.7
-Stage1 += shell(commands=['cd /tmp/', 
-    'wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda-repo-rhel8-11-3-local-11.3.0_465.19.01-1.x86_64.rpm', 
-    'sudo rpm -i cuda-repo-rhel8-11-3-local-11.3.0_465.19.01-1.x86_64.rpm'])
+#  Stage1 += shell(commands=['cd /tmp/', 
+#      'wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda-repo-rhel8-11-3-local-11.3.0_465.19.01-1.x86_64.rpm', 
+#      'sudo rpm -i cuda-repo-rhel8-11-3-local-11.3.0_465.19.01-1.x86_64.rpm'])
 
 
 # Install Python 3.7
-Stage1 += packages(epel=True,
-                   yum=['nvidia-driver:latest-dkms', 'cuda'])
+#  Stage1 += packages(epel=True,
+#                     yum=['nvidia-driver:latest-dkms', 'cuda'])
 
-Stage1 += pip(packages=['keras==2.6.0', 'tensorflow==2.6.0', 'protobuf==3.20.*'], pip='pip3.8')
+#  Stage1 += pip(packages=['keras==2.6.0', 'tensorflow==2.6.0', 'protobuf==3.20.*'], pip='pip3.8')
 
 
